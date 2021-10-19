d1d6ea3544572aa00eb89219e49581aee627cabc
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2021-10-19 20:37:24 -0300

    Update and rename instaladores_gitsetup.sh to git_instaladores.sh

---
+++ git_instaladores.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# script: git_instaladores.sh
+# autor: Luciano Kalabric <luciano.kalabric@fiocruz.br>
+# instituição: Oswaldo Cruz Foundation, Gonçalo Moniz Institute, Bahia, Brazil
+# objetivo: uso do git para clonagem e controle de versão dos scripts
+# criação: 19 OUT 2021
+# ultima atualização: 19 OUT 2021
+# atualização: criação deste script para aprender e lembrar os comandos git
+
+# GitHub CLI
+# Links: https://docs.github.com/pt/github-cli/github-cli/quickstart
+# https://docs.github.com/pt/repositories/creating-and-managing-repositories/cloning-a-repository
+
+# Quick setup — if you’ve done this kind of thing before
+# https://github.com/lkalabric/instaladores.git
+
+# Configure git local
+#git config --global user.name "lkalabric"
+#git config --global user.email "luciano.kalabric@fiocruz.br"
+
+# Clone remote to local (apenas uma vez)
+#git clone https://github.com/lkalabric/instaladores.git /home/brazil1/repos/instaladores
+
+# Commit local
+#git commit -a -m "Atualização do script assembly.sh."
+#git branch -M main
+
+# Push changes to the server
+#git push -u origin main
+
+# Git auth
+# Username: lkalabric
+# Password=personal token: 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
+
+# Pull changes from server
+#git pull
