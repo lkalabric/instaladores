9c3a2c697f819674d6538af4fac3a9dc09c51bd5
lkalabric <luciano.kalabric@fiocruz.br>
2021-10-19 18:39:48 +0000

    first commit

---
+++ instaladores_gitsetup.sh
@@ -0,0 +1,22 @@
+# Configurar git local
+#git config --global user.name "lkalabric"
+#git config --global user.email "luciano.kalabric@fiocruz.br"
+
+# Quick setup — if you’ve done this kind of thing before
+# https://github.com/lkalabric/instaladores.git
+# …or create a new repository on the command line
+#echo "# instaladores" >> README.md
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+# Rodar a próxima linha apenas uma vez
+#git remote add origin https://github.com/lkalabric/instaladores.git
+#git push -u origin main
+
+# …or push an existing repository from the command line
+#git remote add origin https://github.com/lkalabric/instaladores.git
+#git branch -M main
+#git push -u origin main
+# username: lkalabric
+# password=personal token: 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
